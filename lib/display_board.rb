3cd4b0c977fbcdb9f8d92fdf0ef0bb70d9765c98
Arjun Nair <nair.arjun47@yahoo.com>
2019-09-10 21:16:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-columns = "|"
-rows = "-----------"
+
 
 def display_board(board) 
+  
+  columns = "|"
+  rows = "-----------"
+
   puts " #{board[0]}  #{columns} #{board[1]}  #{columns} #{board[2]}  "
   puts "#{rows}"
   puts " #{board[3]}  #{columns} #{board[4]}  #{columns} #{board[5]}  "
